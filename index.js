5ebc640796df86bd4413463496ce5565dacb9669
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-19 15:36:29 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().includes(query.toLowerCase()));
+  }
+
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+  }
+
+  function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query);
+  }
